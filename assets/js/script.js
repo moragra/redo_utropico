1a13b8e6bc6136eb96b39cd4052fb5ab0f067141
moragra <moragra_98@outlook.com>
2023-02-23 13:25:12 -0700

    Upload to github

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+//VA EN PROCESO - crearme un API con todos los productos 
+//llamar los productos dentro del API randomely para que se cambien en la pagina principal
+//TENTATIVO - si se hace mouse over de una imagen entonces usar esa imagen como big picture o se podría dejar así como esta sin el hover
+//dependiendo de la selección del form list
+//si se hace default aparecerá el modal con los default options
+    //escoger cushion 
+        //escoger color
+            //escoger textil 
+                //si no disponible 
+                //hacer input de opcion para el formulario 
+    //escoger tonos
+        //si tonos normales 
+            //escoger madera 
+        //si tonos aclarados
+            //hacer input de opcion de tono para el formulario
+            //madera para el formulario
+    //escoger dimensiones
+        //dimensiones default
+        //dimensiones personalizadas
+    //btn de agregar al carrito once everything is selected
+
+//correo de cotizacion por carrito o personalizado
+    //hacer un correo automatico
+    //con numero de order
+    //datos de compra
+    //datos personales
+
+
+//si se hace default aparecerá el modal con los default options
+// let product_material = document.querySelector("#product_materials")
+// product_material.addEventListener("change", material_selection)
+
+// function material_selection(e){
+//     console.log("hola")
+// }
